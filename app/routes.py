6900d03f2c4d61107c1b60d5610740d849f8f88c
Soeren Fuhr <soren.fuhr@gmail.com>
2013-07-07 16:52:11 +0200

    added 404 page, renamed html files and updated routing

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import os, sys
 import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for
+
 from werkzeug import secure_filename
 
 sys.path.append('./pytesser/')
@@ -17,15 +18,15 @@ app.config.update(
     DEBUG = True,
 )
 
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
@@ -45,10 +46,13 @@ def upload_file():
             print image_string
             return render_template('result.html', result = image_string.decode('utf-8'))
 
-    return render_template('home.html')
+    return render_template('index.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
